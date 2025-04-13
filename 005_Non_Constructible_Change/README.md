2fbb98e7e2144267abea84f3f39b269185642586
Khaled Allam <kaaoaa505@gmail.com>
2025-04-13 11:00:42 +0300

    The Non-Constructible Change algorithm

---
+++ 005_Non_Constructible_Change/README.md
@@ -0,0 +1,10 @@
+# 🧩 Problem Statement:
+- You are given an array of positive integers representing coin denominations.
+- Your task is to determine the minimum amount of change 
+- (the smallest amount of money) 
+- that cannot be created using any combination of these coins.
+
+# 🧠 Example:
+coins = [1, 2, 5]
+- You can create: 1, 2, 3(1+2)
+- The smallest amount you can't make is: 4
---
+++ 005_Non_Constructible_Change/solve_v1.py
@@ -0,0 +1,35 @@
+# ⏱ Time Complexity:
+# Sorting the coins → O(n log n)
+# This is the dominant step. Sorting takes O(n log n) time.
+# Looping through the coins → O(n)
+# Each coin is processed once.
+
+# ✔️ Overall Time Complexity:
+# O(n log n), where n is the number of coins.
+
+# 🧠 Space Complexity:
+# Sorting uses space:
+# Python’s .sort() is in-place for lists → O(1) extra space.
+
+# Other variables:
+# current_change_created is just a single integer → O(1) space.
+
+
+def non_constructible_change(coins):
+    coins.sort()
+    current_change_created = 0
+
+    for coin in coins:
+        if coin > current_change_created + 1:
+            return current_change_created + 1
+        current_change_created += coin
+
+    return current_change_created + 1
+
+
+print(non_constructible_change([1, 2, 5]))  # ➞ 4
+print(non_constructible_change([1, 1, 1, 1, 1]))  # ➞ 6
+print(non_constructible_change([2]))  # ➞ 1
+print(non_constructible_change([1, 1, 2, 3, 5, 7, 22]))  # ➞ 20
+print(non_constructible_change([1, 1, 3]))  # ➞ 6
+print(non_constructible_change([1, 1, 4]))  # ➞ 3
